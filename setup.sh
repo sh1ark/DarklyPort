789e2bb72d0941aac50af0d7db1871599b97f2d8
sh1ark <48487918+sh1ark@users.noreply.github.com>
2019-03-12 17:37:37 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hello Sami Zaidi This Tool Special Foor You ! ;)"
+sleep 2
+echo "Ok Lets Start !"
+mkdir modules
+echo 'Config Files'
+mv gen.sh modules
+echo 'Succesfuly Installed !'
+sleep 1
+python2 dark.py
